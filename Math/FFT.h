34a7c2675452d4235ab5c1dd8c52035d2e081854
Martin Robinson <0x4d52@gmail.com>
2021-01-03 13:00:32 +0000

    Get legacy FFT compiling

--- Math/FFT.h
+++ Math/FFT.h
@@ -1,9 +1,17 @@
-#define	TIME2FREQ			1
-#define	FREQ2TIME			0
+#pragma once
 
-/*Function prototypes for FFT.c*/
-void	InitFFTTable(void);
-void	FFT(float data[], long numberPoints, short direction);
-void	bitreverse(float data[], long numberData);
-void	RealFFT(float data[], long numberPoints, short direction);
+#include "sonicslash_common.h"
+
+namespace sonicslash {
+namespace legacy {
+namespace FFT {
+
+void InitFFTTable(void);
+void FFT(float data[], long numberPoints, short direction);
+void bitreverse(float data[], long numberData);
+void RealFFT(float data[], long numberPoints, short direction);
+    
+} // namespace FFT
+} // namespace legacy
+} // namespace sonicslash
 
