a314dbbd446c913c130a66b641236bfe87ab1c3b
Martin Robinson <0x4d52@gmail.com>
2021-01-03 16:24:55 +0000

    Copy over and re-implement the legacy FindBestRatio() function in the Convolve class

--- SonicSlash/Source/sonicslash_Convolve.cpp
+++ SonicSlash/Source/sonicslash_Convolve.cpp
@@ -1,11 +1,84 @@
-/*
-  ==============================================================================
-
-    sonicslash_Convolve.cpp
-    Created: 3 Jan 2021 3:35:56pm
-    Author:  Martin Robinson
-
-  ==============================================================================
-*/
-
 #include "sonicslash_Convolve.h"
+
+namespace sonicslash {
+
+Convolve::Convolve()
+{
+    
+}
+    
+void Convolve::setBestRatio()
+{
+    long maxInterpolate = windowSize / 8;
+    
+    if (! time)
+    {
+        decimation = interpolation = maxInterpolate;
+        return; // don't change scale
+    }
+
+    float testScale;
+    float percentError = 2.0f;
+    float newScaleFactor;
+    long maxDecimate = maxInterpolate;
+    
+    if (scaleFactor > 1.0f)
+    {
+        for (interpolation = maxInterpolate; percentError > 1.01f; --interpolation)
+        {
+            decimation = (long) ((float) interpolation / scaleFactor);
+            testScale = (float) interpolation / decimation;
+            
+            if (testScale > scaleFactor)
+                percentError = testScale / scaleFactor;
+            else
+                percentError = scaleFactor / testScale;
+            
+            if (percentError < 1.004f)
+                break;
+            
+            if (interpolation == 1)
+            {
+                interpolation = maxInterpolate;
+                decimation = (long) ((float) interpolation / scaleFactor);
+                newScaleFactor = (float) interpolation / decimation;
+                percentError = 1.0f;
+            }
+        }
+    }
+    else
+    {
+        for (decimation = maxDecimate; percentError > 1.01f; --decimation)
+        {
+            interpolation = (long) (decimation * scaleFactor);
+            testScale = (float) interpolation / decimation;
+            
+            if (testScale > scaleFactor)
+                percentError = testScale / scaleFactor;
+            else
+                percentError = scaleFactor / testScale;
+            
+            if (percentError < 1.004f)
+                break;
+            
+            if (decimation == 1)
+            {
+                decimation = maxDecimate;
+                interpolation = (long) (decimation * scaleFactor);
+                newScaleFactor = (float) interpolation / decimation;
+                percentError = 1.0f;
+            }
+        }
+    }
+    
+    scaleFactor = (float) interpolation / decimation;
+}
+
+//void Convolve::setBestRatio()
+//{
+//
+//    scaleFactor = getBestRatio();
+//}
+
+    
+} // namespace sonicslash
--- SonicSlash/Source/sonicslash_Convolve.h
+++ SonicSlash/Source/sonicslash_Convolve.h
@@ -1,14 +1,31 @@
 #pragma once
 
+#include "../../Math/Windows.h"
+
 namespace sonicslash {
     
 class Convolve
 {
 public:
-    
+    Convolve ();
     
 private:
-    
+    void setBestRatio();
+
+    //==========================================================================
+    long points = { 4096 };
+    long halfPoints = { 2048 };
+    long windowSize;
+    long decimation;
+    long interpolation;
+    long windowType = { legacy::HAMMING };
+    bool phaseLocking = { false }; // seems to be always false in legacy code?
+    double scaleFactor;
+    double maskRatio;
+    double minAmplitude = { 0.0 };
+    short analysisType;
+    bool time; // bool?
+    bool useFunction; // function is a 400 element float,.. for UI??
 };
     
 } // namespace sonicslash
