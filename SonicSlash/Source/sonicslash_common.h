31512ae55b92ba8aec302acd46641cca7765bc4d
Martin Robinson <0x4d52@gmail.com>
2021-01-03 13:00:11 +0000

    Add common header for legacy code

---
+++ SonicSlash/Source/sonicslash_common.h
@@ -0,0 +1,18 @@
+#pragma once
+
+#include <JuceHeader.h>
+
+namespace sonicslash
+{
+    
+namespace legacy
+{
+    static constexpr float Pi = juce::MathConstants<float>::pi;
+    static constexpr float twoPi = juce::MathConstants<float>::twoPi;
+
+    static constexpr int TIME2FREQ = 1;
+    static constexpr int FREQ2TIME = 0;
+        
+} // namesapce legacy
+} // namesapce sonicslash
+
