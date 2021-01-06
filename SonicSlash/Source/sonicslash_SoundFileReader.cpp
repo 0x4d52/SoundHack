a08d8f04d9f825fa41c768b0cc36d14c2fed66e5
Martin Robinson <0x4d52@gmail.com>
2021-01-06 01:08:24 +0000

    Implement sample reading in SoundInfo

--- SonicSlash/Source/sonicslash_SoundFileReader.cpp
+++ SonicSlash/Source/sonicslash_SoundFileReader.cpp
@@ -23,5 +23,31 @@ bool SoundInfoReader::open (const File& fileToOpen)
 
     return true;
 }
+    
+long SoundInfoReader::readStereo (long numSamples, float blockL[], float blockR[])
+{
+    if (reader == nullptr)
+        return -1;
+    
+    float* ptrs[] = { blockL, blockR };
+    
+    if (reader->read (ptrs, 2, 0, static_cast<int> (numSamples)))
+        return numSamples;
+    
+    return -1;
+}
+    
+long SoundInfoReader::readMono (long numSamples, float block[])
+{
+    if (reader == nullptr)
+        return -1;
+
+    float* ptrs[] = { block };
+    
+    if (reader->read (ptrs, 1, 0, static_cast<int> (numSamples)))
+        return numSamples;
+    
+    return -1;
+}
 
 } // namespace sonicslash
--- SonicSlash/Source/sonicslash_SoundFileReader.h
+++ SonicSlash/Source/sonicslash_SoundFileReader.h
@@ -13,6 +13,9 @@ public:
     bool open (const File& file) override;
     bool isWriter() const override { return false; }
     
+    long readStereo (long numSamples, float blockL[], float blockR[]) override;
+    long readMono (long numSamples, float block[]) override;
+    
 private:
     
     //==========================================================================
